eb5fa0a4cda9da78517422487f6ac5d873998c5e
tian <jiayuetian@foxmail.com>
2023-07-23 01:25:30 +0900

    popular page

--- animesum/src/Components/Popular.js
+++ animesum/src/Components/Popular.js
@@ -4,17 +4,27 @@ import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 
 
-function Popular() {
-    const {popularAnime, isSearch} = useGlobalContext();
+function Popular({rendered}) {
+    const {popularAnime, isSearch, searchResults} = useGlobalContext();
 
-    const conditionalRender = () => {
-        if(!isSearch)
+    const conditionalRender = () => 
+    {
+        if(!isSearch && rendered === 'popular')
             return popularAnime.map((anime) => {
                 return <Link to={`/anime/${anime.mal_id}`} key={anime.mal_id}>
                     <img src={anime.images.jpg.large_image_url} alt="" />
                     </Link>
-            });
+            })
+        else
+        {
+            return searchResults?.map((anime) => {
+                return <Link to={`/anime/${anime.mal_id}`} key={anime.mal_id}>
+                    <img src={anime.images.jpg.large_image_url} alt="" />
+                </Link>
+            })
+        }
     }
+
     return (
         <PopularStyled>
             <div className="popular-anime">
@@ -30,18 +40,19 @@ const PopularStyled = styled.div`
         margin-top: 2rem;
         padding-top: 2rem;
         padding-bottom: 2rem;
-        padding-left: 5rem;
-        padding-right: 0rem;
+        padding-left: 2rem;
+        padding-right: 0;
         width: 100%;
         display: grid;
-        grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
-        grid-gap: 2rem;
+        grid-template-columns: repeat(auto-fill, minmax(250px, 1fr));
+        grid-gap: 1.5rem;
         background-color: #fff;
-        border-top: 5px solid #1f627f;
+        border-top: 5px solid #e5e7eb;
         a{
-            height: 500px;
+            height: 400px;
+            width: 100%;
             border-radius: 7px;
-            border: 5px solid #78bfde;
+            border: 5px solid #e5e7eb;
         }
         a img{
             width: 100%;
@@ -49,7 +60,6 @@ const PopularStyled = styled.div`
             object-fit: cover;
             border-radius: 5px;
         }
-        
     }
 `;
 
