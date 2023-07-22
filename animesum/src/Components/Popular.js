82dc3f25c01e4903cab81aef14ae29f378032268
tian <jiayuetian@foxmail.com>
2023-07-23 03:57:29 +0900

    popular page

--- animesum/src/Components/Popular.js
+++ animesum/src/Components/Popular.js
@@ -10,7 +10,7 @@ function Popular({rendered}) {
     const conditionalRender = () => 
     {
         if(!isSearch && rendered === 'popular')
-            return popularAnime.map((anime) => {
+            return popularAnime?.map((anime) => {
                 return <Link to={`/anime/${anime.mal_id}`} key={anime.mal_id}>
                     <img src={anime.images.jpg.large_image_url} alt="" />
                     </Link>
